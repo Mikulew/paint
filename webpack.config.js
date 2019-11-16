77fb6893d3c66e6f0d1095055f72b51cb31212e8
Mikulew <mikulew@gmail.com>
2019-11-16 19:47:14 +0100

    Changing webpack's configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,40 @@
-const path = require('path');
+const { resolve } = require('path');
 const webpack = require('webpack');
-
-/*
- * SplitChunksPlugin is enabled by default and replaced
- * deprecated CommonsChunkPlugin. It automatically identifies modules which
- * should be splitted of chunk by heuristics using module duplication count and
- * module category (i. e. node_modules). And splits the chunks…
- *
- * It is safe to remove "splitChunks" from the generated configuration
- * and was added as an educational example.
- *
- * https://webpack.js.org/plugins/split-chunks-plugin/
- *
- */
-
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-/*
- * We've enabled HtmlWebpackPlugin for you! This generates a html
- * page for you when you compile webpack, which will make you start
- * developing and prototyping faster.
- *
- * https://github.com/jantimon/html-webpack-plugin
- *
- */
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
   entry: './src/index.js',
-
   output: {
     filename: '[name].[chunkhash].js',
-    path: path.resolve(__dirname, 'dist'),
+    path: resolve(__dirname, 'dist'),
   },
-
-  plugins: [new webpack.ProgressPlugin(), new HtmlWebpackPlugin()],
-
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'GDPR Plugin',
+      template: './src/index.html',
+      inject: true,
+      minify: {
+        removeAttributeQuotes: true,
+        removeComments: true,
+        collapseWhitespace: true,
+      },
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
   module: {
     rules: [
       {
-        test: /.(js|jsx)$/,
-        include: [path.resolve(__dirname, 'src')],
+        test: /\.js$/,
+        include: [resolve(__dirname, 'src')],
         loader: 'babel-loader',
-
         options: {
           plugins: ['syntax-dynamic-import'],
-
           presets: [
             [
               '@babel/preset-env',
@@ -56,10 +45,21 @@ module.exports = {
           ],
         },
       },
+      {
+        test: [/.css$|.scss$/],
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
+      },
     ],
   },
-
   optimization: {
+    minimizer: [new OptimizeCssAssetsPlugin(), new TerserPlugin()],
     splitChunks: {
       cacheGroups: {
         vendors: {
@@ -67,14 +67,12 @@ module.exports = {
           test: /[\\/]node_modules[\\/]/,
         },
       },
-
       chunks: 'async',
       minChunks: 1,
       minSize: 30000,
       name: true,
     },
   },
-
   devServer: {
     open: true,
   },
